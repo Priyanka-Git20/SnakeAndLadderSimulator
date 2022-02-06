a0a4c68ac236af4c10a4d73ffd6b8c825e294cb5
priyanka <priyanka.chavan1408@gmail.com>
2022-02-06 13:35:33 +0530

    ADDED:After rolling the dice get number between 1 to 6.

---
+++ SnakeAndLadderSimulatorUC2.java
@@ -0,0 +1,10 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderSimulatorUC2 {
+    public static void main(String[] args) {
+        int START_POSITION = 0;
+        System.out.println("Player start at position " +START_POSITION );
+        int ROLL_THE_DICE = (int) (Math.floor(Math.random() *10) %6 ) +1;
+        System.out.println("After rolling the dice we get " +ROLL_THE_DICE);
+    }
+}
