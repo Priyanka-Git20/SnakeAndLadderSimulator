34e3836a197c19c1abc8c1a81a44a70fbe7bd42c
priyanka <priyanka.chavan1408@gmail.com>
2022-02-06 13:32:50 +0530

    ADDED:Game played with single player and start position is 0.

---
+++ SnakeAndLadderSimulatorUC1.java
@@ -0,0 +1,8 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderSimulatorUC1 {
+    public static void main(String[] args) {
+        int START_POSITION = 0;
+        System.out.println("Player start at position " +START_POSITION );
+    }
+}
