ed7203f4fd2b42699685fa3ec11ef87ee37e2be6
priyanka <priyanka.chavan1408@gmail.com>
2022-02-06 13:29:02 +0530

    ADDED:Welcome message on the master branch.

---
+++ SnakeAndLadderSimulator.java
@@ -0,0 +1,7 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake and ladder simulation program.");
+    }
+}
